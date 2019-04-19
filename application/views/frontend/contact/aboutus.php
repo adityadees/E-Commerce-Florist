ed8eb8f97789773340acd3dafa1dbc28f060f723
AdityaDS <adityads@ymail.com>
2019-04-19 12:36:21 +0700

    Initial commit

---
+++ application/views/frontend/contact/aboutus.php
@@ -0,0 +1,66 @@
+        <div class="breadcrumbs_area bread_about">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="breadcrumb_content">
+                            <div class="breadcrumb_header">
+                                <a href="index.html"><i class="fa fa-home"></i></a>
+                                <span><i class="fa fa-angle-right"></i></span>
+                                <span> About Us</span>
+                            </div>
+                            <div class="breadcrumb_title">
+                                <h2>About Us</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="about_section_aera">
+         <div class="container-fluid about_container">
+             <div class="row no-gutters align-items-center">
+                 <div class="col-lg-12 col-md-12">
+                     <div class="about_content">
+                        <h1>Sejarah <br>Gantri Minimarket</h1>
+                        <p>Sejalan dengan visi misi SMK Negeri 3 Palembang sebagai sekolah yang berbasis bisnis dan manajemen, maka pada bulan Mei 2015 dimusyawarakanlah antara Para Wakil Kepala Sekolah dan para Ketua Kompetensi dan Pemegang jabatan Unit produksi di SMK Negeri 3 Palembang untuk membuat salah satu wadah untuk tempat anak melakukan kegiatan industri pada dunia industri yang nyata.
+                            Hasil permusyawaratan tersebut akhirnya menunjuk KPRI GUKA SMK NEGERI 3 Palembang sebagai wadah koperasi guru- guru dan karyawan untuk mendirikan sebuah minimarket, yang pada saat itu permodalan berasal dari koperasi, unit produksi, koperasi pelajar dan dari sisa uang bisnis centre smk negeri 3 yang lama.
+                            Minimarket tersebut merupakan waralaba dengan PT INTI CAKRAWALA CITRA yang kantor pusatnya ada di Jakarta, dengan perwakilan distribusi barang dan kantor cabangnya ada di Palembang. Usaha minimarket ini sesuai standar pertokoan yang ada di dunia industri sehingga peserta didik yang berasal dari berbagai kompetensi keahlian dapat belajar di minimarket ini, terutama peserta didik dari paket keahlian Bisnis daring dan Pemasaran sebagai “ Teaching Factory” mereka sebelum mereka diterjunkan ke tengah masyarakat menghadapin dunia industri.
+                        </p> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="counter_up_area mb-40">
+      <div class="container-fluid counter">
+          <div class="row no-gutters">
+              <div class="col-lg-6 col-md-6 col-sm-6">
+                  <div class="single_counterup">
+                   <div class="counter_img">
+                    <img src="assets/img/cart/count.png" alt="">
+                </div>
+                <div class="counter_info">
+                    <h2 class="counter_number text-center">4</h2>
+                    <p>Jumlah Pekerja</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6">
+          <div class="single_counterup count-two">
+            <div class="counter_img">
+                <img src="assets/img/cart/count2.png" alt="">
+            </div>
+            <div class="counter_info">
+                <h2 class="counter_number text-center">19</h2>
+                <p>Jam Kerja</p>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+</div>
+
---
+++ application/views/frontend/contact/contactus.php
@@ -0,0 +1,55 @@
+<!--breadcrumbs area start-->
+<div class="breadcrumbs_area contact_bread contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="breadcrumb_content">
+                    <div class="breadcrumb_header">
+                        <a href="index.html"><i class="fa fa-home"></i></a>
+                        <span><i class="fa fa-angle-right"></i></span>
+                        <span> Contact</span>
+                    </div>
+                    <div class="breadcrumb_title">
+                        <h2>Contact</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--breadcrumbs area end-->
+
+<div class="contact_area mb-40">
+ <div class="container">
+     <div class="row">
+         <div class="col-lg-6 col-md-12">
+             <div class="contact_message">
+                <div class="contact_title">
+                    <h2>Our Locations</h2>   
+                </div>
+
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d704.3483402977597!2d104.72912110453498!3d-2.9874922095235674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x53e086977ac55e22!2sGantri!5e0!3m2!1sid!2sid!4v1545066678537" width="auto" height="450" frameborder="0" style="border:0" allowfullscreen class="col-md-12"></iframe>
+            </div> 
+        </div>
+        <div class="col-lg-6 col-md-12">
+         <div class="contact_info_wrapper">
+            <div class="contact_title">
+                <h4>contact us</h4>    
+            </div>
+           <div class="contact_info mb-15">
+            <ul>
+                <li><i class="fa fa-fax"></i>  Address : Jalan Srijaya Negara Bukit Besar Palembang Kelurahan Bukit Lama  Kecamatan Ilir Barat I Kota Palembang Provinsi Sumatera Selatan Kode  Pos 30139.</li>
+                <li><i class="fa fa-phone"></i> <a href="#">(0711) 351816</a></li>
+                <li><i class="fa fa-whatsapp"></i> <a href="#">+62 818 0406 6060</a></li>
+                <li><i class="fa fa-envelope-o"></i> Gantriminimarket@gmail.com</li>
+            </ul>        
+        </div>
+        <div class="contact_info mb-15">
+            <h3><strong>Working hours</strong></h3>
+            <p><strong>Monday – Sunday</strong>:  08AM – 22PM</p>    
+        </div>    
+    </div> 
+</div>
+</div>
+</div>
+</div>
